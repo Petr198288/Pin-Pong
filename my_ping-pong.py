d57fda43d1708dc77e4e4e9b4dde883bc0479041
Melstroy Magomedow <petrkoleso2011@mail.ru>
2025-05-18 11:45:38 +0300

    Добавлен фон

---
+++ my_ping-pong.py
@@ -0,0 +1,96 @@
+from pygame import *
+from random import randint
+from time import time as timer
+#подгружаем отдельно функции для работы со шрифт
+
+
+#фоновая музыка
+
+#нам нужны такие картинки:
+img_back = "плоская-таблица-pong-pin-взгляд-сверху-поля-пингпонга-с-линией-вектором-118501901.webp" #фон игры
+img_hero = "racket.png" #герой
+img_enemy = "png-transparent-tennis-balls-yellow-sporting-goods-tennis-ball-icon-miscellaneous-sport-sports-equipment-thumbnail.png" #враг
+
+
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+ 
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+ 
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+gun_clip = 5
+#класс главного игрока
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.x > -800:
+            self.rect.x += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x > -800:
+            self.rect.x += self.speed
+        
+
+       
+
+
+#класс спрайта-врага  
+class Enemy(GameSprite):
+    #движение врага
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        #исчезает, если дойдёт до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+ 
+
+
+
+#создаём окошко
+win_width = 700
+win_height = 500
+display.set_caption("Ping-Pong")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+#создаём спрайты
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+#основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+clock = time.Clock()
+Reload = False
+while run:
+    
+    #событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        #событие нажатия на пробел - спрайт стреляет
+
+    window.blit(background, (0,0))
+    clock.tick(40)
+    display.update()
